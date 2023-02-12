905426728e3bcfa3a70c151a1fc1b9382805db45
Jigar Mehta <jigarmehta277@gmail.com>
2023-02-12 17:09:18 -0500

    CRUD operations for valorantUser

---
+++ valorantApplication/Controllers/valorantUsersDataController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using valorantApplication.Models;
+using HttpGetAttribute = System.Web.Http.HttpGetAttribute;
+using HttpPostAttribute = System.Web.Http.HttpPostAttribute;
+
+namespace valorantApplication.Controllers
+{
+    public class valorantUsersDataController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/valorantUsersData/ListValorantUsers
+        [HttpGet]
+        public IEnumerable<ValorantUserDto> ListvalorantUsers()
+        {
+            List<valorantUser> ValorantUsers = db.valorantUsers.ToList();
+            List<ValorantUserDto> ValorantUserDtos = new List<ValorantUserDto>();
+
+            ValorantUsers.ForEach(a => ValorantUserDtos.Add(new ValorantUserDto()
+            {
+                UserId = a.UserId,
+                UserName = a.UserName,
+                ValorantID = a.ValorantID,
+                MostPlayedAgent = a.MostPlayedAgent,
+                PlayHours = a.PlayHours,
+                Scale = a.Scale,
+                Reason = a.Reason,
+                Skill = a.Skill,
+                Position = a.Position,
+                Strategies = a.Strategies,
+                Ranks = a.Ranks
+            }));
+
+            return ValorantUserDtos;
+        }
+
+        // GET: api/valorantUsersData/FindValorantUser/5
+        [ResponseType(typeof(valorantUser))]
+        [HttpGet]
+        public IHttpActionResult FindValorantUser(int id)
+        {
+            valorantUser valorantUser = db.valorantUsers.Find(id);
+            ValorantUserDto valorantUserDto = new ValorantUserDto()
+            {
+                UserId = valorantUser.UserId,
+                UserName = valorantUser.UserName,
+                ValorantID = valorantUser.ValorantID,
+                MostPlayedAgent = valorantUser.MostPlayedAgent,
+                PlayHours = valorantUser.PlayHours,
+                Scale = valorantUser.Scale,
+                Reason = valorantUser.Reason,
+                Skill = valorantUser.Skill,
+                Position = valorantUser.Position,
+                Strategies = valorantUser.Strategies,
+                Ranks = valorantUser.Ranks
+            };
+            if (valorantUser == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(valorantUserDto);
+        }
+
+        // POST: api/valorantUsersData/UpdateValorantUser/5
+        [ResponseType(typeof(void))]
+        [HttpPost]
+        public IHttpActionResult UpdateValorantUser(int id, valorantUser valorantUser)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != valorantUser.UserId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(valorantUser).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!valorantUserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/valorantUsersData/AddValorantUser
+        [ResponseType(typeof(valorantUser))]
+        [HttpPost]
+        public IHttpActionResult AddValorantUser(valorantUser valorantUser)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.valorantUsers.Add(valorantUser);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = valorantUser.UserId }, valorantUser);
+        }
+
+        // POST: api/valorantUsersData/DeleteValorantUser/5
+        [ResponseType(typeof(valorantUser))]
+        [HttpPost]
+        public IHttpActionResult DeletevalorantUser(int id)
+        {
+            valorantUser valorantUser = db.valorantUsers.Find(id);
+            if (valorantUser == null)
+            {
+                return NotFound();
+            }
+
+            db.valorantUsers.Remove(valorantUser);
+            db.SaveChanges();
+
+            return Ok();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool valorantUserExists(int id)
+        {
+            return db.valorantUsers.Count(e => e.UserId == id) > 0;
+        }
+    }
+}
--- valorantApplication/Models/valorantUser.cs
+++ valorantApplication/Models/valorantUser.cs
@@ -35,4 +35,29 @@ namespace valorantApplication.Models
 
         public ICollection<TournamentDetails> TournamentDetails { get; set; }
     }
+
+    public class ValorantUserDto
+    {
+        public int UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public string ValorantID { get; set; }
+
+        public string MostPlayedAgent { get; set; }
+
+        public string PlayHours { get; set; }
+
+        public int Scale { get; set; }
+
+        public string Reason { get; set; }
+
+        public string Skill { get; set; }
+
+        public string Position { get; set; }
+
+        public string Strategies { get; set; }
+
+        public string Ranks { get; set; }
+    }
 }
